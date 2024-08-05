9b269a3309b6cb5e60bfe5627e0bdcd0971d1b43
Kevin Schwartz <kevosoup@gmail.com>
2024-08-05 16:44:12 -0400

    finished API, began axios

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header"
 import Posts from "../components/Posts"
 import Sidebar from "../components/Sidebar"
+import axios from "axios";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () =>{
+      axios.get("")
+    }
+  }, []);
   return (
     <>
         <Header />
